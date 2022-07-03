a141a2d945e037d5b99bbe1b797e6c6a25d99061
Andrey <andreysozdatelev@gmail.com>
2022-07-03 17:58:06 +0300

    Переменные

---
+++ Basic/Basic.js
@@ -0,0 +1,27 @@
+// summary of https://www.youtube.com/watch?v=Bluxbh9CaQ0
+// 1. Переменные
+// variable
+// var name = 'Andrey' // можно менять string
+// const lastname = 'Sozdatelev' //если не меняется
+// let age = 37 // можно менять number
+// name = 'andrew'
+
+// console.log(name)
+// console. log(lastname)
+// age = 29
+// console.log(age)
+
+const isProgrammer = true // boolean
+const firstName = 'Andrey'
+const lastname = 'Sozdatelev'
+const age = 37
+
+const _private = 'private'
+const $ = 'some value'
+// const if = 'sdf' err
+//const 5number = 5 // err
+
+// 2. Мутирование
+//console.log('Имя человека: ' + firstName + ' , а возраст человека:' + age)
+
+// 3. Операторы
