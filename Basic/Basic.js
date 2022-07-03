c8c2f2fea7ea8939fb35b204b35140e95e1cc35a
Andrey <andreysozdatelev@gmail.com>
2022-07-03 18:26:28 +0300

    typeof

--- Basic/Basic.js
+++ Basic/Basic.js
@@ -11,13 +11,13 @@
 // age = 29
 // console.log(age)
 
-const isProgrammer = true // boolean
-const firstName = 'Andrey'
-const lastname = 'Sozdatelev'
-const age = 37
+// const isProgrammer = true // boolean
+// const firstName = 'Andrey'
+// const lastname = 'Sozdatelev'
+// const age = 37
 
-const _private = 'private'
-const $ = 'some value'
+// const _private = 'private'
+// const $ = 'some value'
 // const if = 'sdf' err
 //const 5number = 5 // err
 
@@ -25,3 +25,42 @@ const $ = 'some value'
 //console.log('Имя человека: ' + firstName + ' , а возраст человека:' + age)
 
 // 3. Операторы
+// const currentYear = 2022
+// const birthYear = 1984
+// const currentAge = currentYear - birthYear
+// console.log(currentAge)
+// const a = 10
+// const b = 5
+// let c = 5
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a*b)
+// console.log(a/b)
+// //console.log(c++) //старое значение, потом прибавляем
+// //console.log(++c) // сначала прибавляем, потом выводим
+// //console.log(c--) // сначала выводим старое значение, потом уменьшаем на 1
+// // console.log(--c) // уменгьшаем на 1, выводим значение
+// //c = c + a 
+// c += a // Тожесамое что c = c + a
+// console.log(c)
+
+// 4. Типы данных
+
+// const isProgrammer = true 
+// const firstName = 'Andrey'
+// const lastname = 'Sozdatelev'
+// const age = 37
+// let x
+// console.log(typeof(isProgrammer))
+// console.log(typeof(firstName))
+// console.log(typeof age)
+// console.log(typeof x)
+// console.log(typeof null)
+
+// 5. Приоритет операторов
+const fullAge = 38
+const birthYear = 1984
+const currentYear = 2022
+//<; >; <=; >=
+const isFullAge = currentYear - birthYear >= fullAge
+console.log(isFullAge)
