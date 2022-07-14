a5cf40dbdce9eb384d8b1a55aca3c1044eaecd93
Andrey <andreysozdatelev@gmail.com>
2022-07-14 17:40:04 +0300

    infinity

---
+++ Basic/numbers.js
@@ -0,0 +1,15 @@
+// Number
+const num = 25 // integer
+console.log(num)
+const float = 25.25 //float
+console.log(float)
+const pow = 10e3 // 10 в 3 степени
+console.log(pow)
+console.log('MAX_SAFE_INTEGER' , Number.MAX_SAFE_INTEGER) // max number javascript над которым может производить действия
+console.log('Math.pow 53' , Math.pow(2, 53) - 1)
+console.log("MIN_SAFE_INTEGER" ,Number.MIN_SAFE_INTEGER)
+console.log("Max_Value", Number.MAX_VALUE) // мах число которое воспринимает javascript
+console.log("Min Value", Number.MIN_VALUE)
+console.log("POSITIVE_INFINITY", Number.POSITIVE_INFINITY) // позитивная бесконечность
+console.log("NEGATIVE_INFINITY", Number.NEGATIVE_INFINITY)
+console.log("1 / 0 = ", 1/0) // бесконечность
