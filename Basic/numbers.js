55502c664a9ef3d2b97c118930e382fccb04498b
Andrey <andreysozdatelev@gmail.com>
2022-07-14 18:10:19 +0300

    parseInt, parseFloat

--- Basic/numbers.js
+++ Basic/numbers.js
@@ -1,15 +1,33 @@
-// Number
-const num = 25 // integer
-console.log(num)
-const float = 25.25 //float
-console.log(float)
-const pow = 10e3 // 10 в 3 степени
-console.log(pow)
-console.log('MAX_SAFE_INTEGER' , Number.MAX_SAFE_INTEGER) // max number javascript над которым может производить действия
-console.log('Math.pow 53' , Math.pow(2, 53) - 1)
-console.log("MIN_SAFE_INTEGER" ,Number.MIN_SAFE_INTEGER)
-console.log("Max_Value", Number.MAX_VALUE) // мах число которое воспринимает javascript
-console.log("Min Value", Number.MIN_VALUE)
-console.log("POSITIVE_INFINITY", Number.POSITIVE_INFINITY) // позитивная бесконечность
-console.log("NEGATIVE_INFINITY", Number.NEGATIVE_INFINITY)
-console.log("1 / 0 = ", 1/0) // бесконечность
+// // Number
+// const num = 25 // integer
+// console.log(num)
+// const float = 25.25 //float
+// console.log(float)
+// const pow = 10e3 // 10 в 3 степени
+// console.log(pow)
+// console.log('MAX_SAFE_INTEGER' , Number.MAX_SAFE_INTEGER) // max number javascript над которым может производить действия
+// console.log('Math.pow 53' , Math.pow(2, 53) - 1)
+// console.log("MIN_SAFE_INTEGER" ,Number.MIN_SAFE_INTEGER)
+// console.log("Max_Value", Number.MAX_VALUE) // мах число которое воспринимает javascript
+// console.log("Min Value", Number.MIN_VALUE)
+// console.log("POSITIVE_INFINITY", Number.POSITIVE_INFINITY) // позитивная бесконечность
+// console.log("NEGATIVE_INFINITY", Number.NEGATIVE_INFINITY)
+// console.log("1 / 0 = ", 1/0) // бесконечность
+// console.log(Number.NaN) // Not A Number
+// console.log(typeof NaN)
+// const primerNaN = 2 / undefined
+// console.log(isNaN(primerNaN))
+// console.log(Number.isNaN(25))
+// console.log(Number.isFinite(Infinity)) // является ли число конечным
+// console.log(Number.isFinite(25))
+
+// const stringInt = "42"
+// const stringFloat = "42.42"
+// console.log(stringInt + 2) // конкатенация 422
+// console.log(Number.parseInt(stringInt) + 2) // парсим число из строки = 44
+// console.log(parseInt(stringInt) + 2) 
+// console.log(Number(stringInt) + 2) // 42 + 2
+// console.log(+stringInt + 2) // +(строка) преобразует строчку в число
+
+// console.log(Number.parseInt(stringFloat) + 2) // отсекает нецелочисленное значение
+// console.log(Number.parseFloat(stringFloat) + 2) // работает с нецелыми числами
