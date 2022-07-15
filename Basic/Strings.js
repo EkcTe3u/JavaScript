7e80dee21ff922a9aef24a6303e229858b6379bc
Andrey <andreysozdatelev@gmail.com>
2022-07-15 15:03:40 +0300

    base

---
+++ Basic/Strings.js
@@ -0,0 +1,20 @@
+// // Strings
+// const firstName = 'Андрей'
+// console.log(typeof firstName)
+// const age = 37
+
+// function getAge() {
+//     return age
+// }
+// // // const output = "Привет, меня зовут " + name + " и мой возраст " + age + " лет." // легко запутаться если много переменных
+// // //const output = `Привет, меня зовут ${firstName} и мой возраст ${getAge()} лет` 
+// // const output = `Привет, меня зовут ${firstName} и мой возраст ${age < 30 ? 'A' : 'B'} лет` // можем использовать тернарные выражения
+// // console.log(output)
+// const output = `
+//     <div> this is div</div>
+//                                                  <p> this is p</p>
+// ` // можем писать в несколько строк, сохраняются все отступы, переходы
+// console.log(output)
+
+// внутренние методы
+const firstName = 'Андрей'
