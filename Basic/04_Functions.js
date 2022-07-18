de47c7a53a0ef6dbea64b91f443d1e6000dfc8d1
Andrey <andreysozdatelev@gmail.com>
2022-07-18 11:51:33 +0300

    declaration, expression

---
+++ Basic/04_Functions.js
@@ -0,0 +1,20 @@
+// Функции
+// Function Declaration можем обращаться когда захотим
+// function greet(name) {
+//     console.log("Привет - ", name)
+// }
+// Function Expression сначада задается, потом вызывается
+// const greet2 = function greet2(name) {
+//     console.log("Привет 2 - ",name)
+// }
+// greet('Andrey')
+// greet2('Andrey')
+// console.log(typeof greet) // для определения функция это или переменная
+// console.dir(greet) // функции являются объектами
+
+// Анонимные функциии
+
+// let counter = 0
+// setInterval(function(){
+//     console.log(++counter)
+// }, 1000)
