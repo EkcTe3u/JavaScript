3381252f12ca0c84f4e3f15ee1adefe7d4ed3ff4
Andrey <andreysozdatelev@gmail.com>
2022-07-20 14:13:22 +0300

    array

---
+++ Basic/05_Arrays.js
@@ -0,0 +1,37 @@
+// Массивы
+const cars = ['Мазда', 'Форд', 'БМВ', 'Мерседес']
+const fib = [1, 1, 2, 3, 5, 8, 13] // можем хранить числа, строки, булевые значения, объекты  и тд
+const people = [
+    {name: 'Andrey', budget: 5000},
+    {name: 'Kristina', budget: 2000},
+    {name: 'Gleb', budget: 500}
+]
+// console.log(cars)
+// console.log(cars.length)
+// //Function
+// function addItemToEnd(){
+
+// }
+// // Method
+// cars.push('Рено') // at the end
+
+// console.log(cars)
+
+// cars.unshift('Волга') // at the beginning
+// console.log(cars)
+// cars.pop() // at the end, также можно занести в переменную
+// cars.shift() // delete at the beginning, также можно занести в переменную 
+// // const firtsItem = cars.shift()
+// // const lastItem = cars.pop()
+// console.log(cars)
+// console.log(cars.reverse()) // переворачивает массив
+console.log(cars.indexOf('БМВ')) // возвращает индекс элемента, который мы ищем. строки, числа 
+const index = cars.findIndex()
+
+
+
+// Задача 1
+// const text = "Привет, мы изучаем JavaScript"
+// const reverseText = text.split("").reverse().join('')
+// console.log(reverseText)
+
