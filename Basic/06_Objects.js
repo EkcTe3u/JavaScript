e6805ab63e11718002e57ca9992bbedc67c63c1b
Andrey <andreysozdatelev@gmail.com>
2022-07-21 15:51:18 +0300

    contex

--- Basic/06_Objects.js
+++ Basic/06_Objects.js
@@ -4,10 +4,13 @@ const person = { // ключ = значение
     age: 37,
     isQAEngineer: true,
     languages: ['ru', 'en'],
-    'complex key': 'Complex Value',
-    ['key_' + (1 +3)]: '', // [] - вычисляемое значение
+    // 'complex key': 'Complex Value',
+    // ['key_' + (1 +3)]: '', // [] - вычисляемое значение
     greet ()    /* или :function() */ {
         console.log('greet from person')
+    },
+    info(){
+        console.info('Информация о человеке по имени: ', this.name) // person.name = this.name
     }
 }
 
@@ -27,4 +30,30 @@ const person = { // ключ = значение
 // const languages = person.languages
 // const {name, age, languages} = person // age: personAge = 10 - поменять имя переменной, назначить значение по умолчанию
 // console.log(name, age, languages)
+// for (let key in person){
+//     console.log('key: ',key)
+//     console.log('value: ', person[key])
+// }
+// const keys = Object.keys(person) // не бежит по прототипу, hasOwnProperty
+// keys.forEach((key) => {
+//     console.log('key: ',key)
+//     console.log('value: ', person[key])
+// })
 
+// Context
+// person.info()
+
+const logger = {
+    keys(obj){
+        console.log('Object Keys:', Object.keys(this))
+    },
+    keysAndValues(){
+        Object.keys(this).forEach(key =>{
+            console.log(`"${key}": ${this[key]}`)
+        })
+    }
+}
+// const bound = logger.keys.bind(person) // this будет тем объектом, который мы сами передадим
+// bound()                 // bind создает функцию
+logger.keys.call(person) //  сразу вызывает
+logger.keysAndValues.call(person)
