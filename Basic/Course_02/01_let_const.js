00835ad64f4bce40f2962ed7fa81c0a25962fd8a
Andrey <andreysozdatelev@gmail.com>
2022-12-18 14:42:03 +0300

    01_let_const

---
+++ Basic/Course_02/01_let_const.js
@@ -0,0 +1,42 @@
+// var num = 24;
+// num = 'string';
+// console.log(num)
+
+// if (true) {
+//     var a = 42
+// }
+// console.log(a)
+// let b = 22
+// if (true) {
+//     //let only in {} 
+//     let b = 42
+//     console.log('b', b)
+// }
+// console.log(b)
+
+// Hoisting
+
+// b = 20
+// console.log(b)
+
+// let b = 5
+// function hosted () {
+//     age = 37
+// }
+// let age
+// hosted()
+// console.log(age)
+
+// Const
+
+const color = "hiiuui"
+// color = "gjhgjhg"
+console.log(color)
+const array = [1, 2, 3, 5, 8]
+console.log(array)
+array.push(13)
+console.log("array13", array)
+
+const obj = {a: 42}
+obj.name = "Andrey"
+console.log("obj", obj)
