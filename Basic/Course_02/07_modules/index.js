8aa1534fa871aaef5ae6177691d118eddf46dead
Andrey <andreysozdatelev@gmail.com>
2023-01-03 15:56:14 +0300

    07_modules

---
+++ Basic/Course_02/07_modules/index.js
@@ -0,0 +1,9 @@
+// import logger, {color, compute} from './modules'
+
+// compute (1, 2)
+
+// logger.log()
+
+import * as Module from './modules'
+Module.color
+Module.default.log()
---
+++ Basic/Course_02/07_modules/modules.js
@@ -0,0 +1,13 @@
+const priveteVariable = 42
+
+export const color = "#bababa"
+
+export function compute (a, b) {
+    return a + b
+}
+
+export default {
+    log() {
+        console.log(priveteVariable)
+    }
+}
