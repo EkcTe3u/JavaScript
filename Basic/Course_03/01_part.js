fba493fef5c8e020b5daa339382340a2cae85714
Andrey <andreysozdatelev@gmail.com>
2023-01-05 23:35:57 +0300

    part_01

---
+++ Basic/Course_03/01_part.js
@@ -0,0 +1,8 @@
+// assert (a == 1, " Di saster ! а is not 1!" ) ;
+
+// start = new Date().getTime();
+// for (var n = 0; n < maxCount ; n++ ){
+// //  выполнить измеряемую операцию
+// }
+// elapsed = new Date().getTime() - start;
+// assert (true , "Measured time: "+ elapsed) ;
