a1a56775f6a8268c0264371ce538d5b8206aa61d
Andrey <andreysozdatelev@gmail.com>
2023-01-07 15:15:12 +0300

    part_04

---
+++ Basic/Course_03/04_part.js
@@ -0,0 +1,7 @@
+// function isPalindrome (text) {
+//     if (text.length <= 1) return true ;
+//     if (text.charAt (0) != text.charAt ( text.length - 1 ) ) return false ;
+//     return isPalindrome ( text.substr (1 ,text.length - 2 ) );
+// }
+
+
