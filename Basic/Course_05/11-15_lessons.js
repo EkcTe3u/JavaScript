1026c2b5266944bb16498c61b236086258f42e86
Andrey <andreysozdatelev@gmail.com>
2023-01-08 19:18:22 +0300

    11-15_lessons

---
+++ Basic/Course_05/11-15_lessons.js
@@ -0,0 +1,36 @@
+//  lesson 11
+
+console.log(-0.304);
+
+// lesson 12
+
+let firstName = 'Alexander';
+// Код выполнится без ошибок
+firstName[0] = 'B';
+console.log(firstName);
+
+const one = 'Naharis';
+const two = 'Mormont';
+const three = 'Sand';
+
+console.log(one[2]+two[1]+three[3]+two[4]+two[2])
+
+// lesson 13
+
+console.log()
+const soldiersCount = -2000
+console.log(Math.abs(soldiersCount));
+
+// lesson 14
+
+const number = 125.125
+console.log(Math.ceil(number));
+
+// lesson 15
+
+const text = 'Never forget what you are, for surely the world will not';
+
+// BEGIN (write your solution here)
+
+console.log('First: ' + text[0]);
+console.log('Last: ' + text[text.length - 1])
