89cd2668d0fbd40ddfafd72f94074548d4d383e3
Andrey <andreysozdatelev@gmail.com>
2023-01-08 17:18:17 +0300

    1-10_lessons

---
+++ Basic/Course_05/01-10_lessons.js
@@ -0,0 +1,68 @@
+// lesson 1
+console.log("Hello World!")
+
+/* some comments */
+
+// som comments
+
+// lesson 2
+console.log('Robert');
+console.log('Stannis');
+console.log('Renly');
+
+// lesson 3
+console.log(3 ** 5);
+console.log(-8 / -4);
+console.log(100 % 3);
+console.log((3 ** 5) + (-8 / -4) + (100 % 3));
+
+// lesson 4 - mistakes
+
+// console.log(3 ** (4 - 2)
+
+// lesson 5
+
+console.log("\\ \\ \\\\ \\\ \'\"");
+console.log('----------------');
+console.log('- Are you hungry?\n- Aaaarrrgh!');
+console.log('----------------');
+console.log('Gregor Clegane\nDunsen\nPolliver\nChiswyck');
+console.log('----------------');
+console.log('Polliver');
+console.log('Gregor Clegane');
+console.log();
+console.log('Chiswyck');
+console.log('\n');
+console.log('Dunsen');
+
+console.log('- Did Joffrey agree?\n- He did. He also said "I love using \\n".');
+
+// lesson 6
+
+let pet = 'Dragon';
+
+// lesoon 7
+
+let who = "dragon's " + 'mother';
+console.log(who);
+
+const eurosCount = 100;
+console.log(eurosCount * 1.25)
+console.log(eurosCount * 1.25 * 60)
+
+// lesson 8
+
+const king = 'King Balon the 6th';
+const numberOfCastles = 6;
+const roomsPerCastle = 17;
+console.log(king + ' has ' + numberOfCastles * roomsPerCastle + ' rooms.');
+
+// lesoon 9
+
+const stark = 'Andrey'
+console.log(`Do you want to eat, ${stark}?`);
+
+// lesson 10
+
+const magic = '\nyou'
+console.log(magic[1]); // => y
