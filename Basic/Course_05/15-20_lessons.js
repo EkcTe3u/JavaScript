9b5688d65bb8c3a49f68af6ae7b23a0ddfc59f6f
Andrey <andreysozdatelev@gmail.com>
2023-01-08 20:19:55 +0300

    15-20_lessons

---
+++ Basic/Course_05/15-20_lessons.js
@@ -0,0 +1,35 @@
+// lesson 16
+
+console.log(Math.min(3, -3, 10, 22, 0));
+
+// lesson 17
+
+console.log(Math.round(Math.random() * 10));
+
+// lesson 18
+
+const motto = 'abra kadabra'
+console.log(typeof motto);
+
+// lesson 19
+
+let company = 'Hexlet';
+console.log(company.toLowerCase().toUpperCase());
+
+let firstName = '  Grigor   \n';
+
+// BEGIN (write your solution here)
+console.log(firstName.trim())
+
+// lesson 20
+
+const peopleCount = 5;
+console.log(peopleCount.toString())
+
+const name11 = 'Tirion';
+console.log(name11.length.toString());
+console.log(name11.toUpperCase().toLowerCase().length.toString().length);
+
+const text = 'When \t\n you play a \t\n game of thrones you win or you die.';
+
+console.log(text.slice(5, 15).trim().length)
