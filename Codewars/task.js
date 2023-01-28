2212294e12501262a199366513b2ee6f58e9eb65
Andrey <andreysozdatelev@gmail.com>
2023-01-28 14:01:32 +0300

    add task

--- Codewars/task.js
+++ Codewars/task.js
@@ -27,4 +27,31 @@ function digitalRoot(n) {
 }
 console.log(digitalRoot(132189));
 
+// In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number   
+// highAndLow("1 2 3 4 5");  // return "5 1"
+// highAndLow("1 2 -3 4 5"); // return "5 -3"
+
+function highAndLow(numbers){
+    numbers = numbers.split(' ');
+    let lowNumber = Number(numbers[0]);
+    let highNumber = Number(numbers[0]);
+    for(i = 0; i < numbers.length; i++) {
+        if(lowNumber <= Number(numbers[i])){
+            this.lowNumber = lowNumber;
+        } else{
+            lowNumber = numbers[i];
+        }
+        if(highNumber >= Number(numbers[i])){
+            this.highNumber = highNumber;
+        }
+        else{
+            highNumber = numbers[i];
+        }
+    }
     
+    return `${highNumber} ${lowNumber}`;
+}
+
+console.log(highAndLow('8 3 -5 42 -1 0 0 -9 4 7 4 -4'))
+console.log(highAndLow('1 2 3'))
+console.log(typeof(highAndLow('1 9 3 4 -5')))
