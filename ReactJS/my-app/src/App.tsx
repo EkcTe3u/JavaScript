0277e1ba8f02cd63d22f72891e076a48dd32a7fa
Andrey <andreysozdatelev@gmail.com>
2023-02-07 21:53:18 +0300

    context

--- ReactJS/my-app/src/App.tsx
+++ ReactJS/my-app/src/App.tsx
@@ -4,15 +4,17 @@ import { Loader } from './components/Loader';
 import { ErrorMessage } from './components/ErrorMessage';
 import { Modal } from './components/Modal';
 import { CreateProduct } from './components/CreateProduct';
-import { useState } from 'react';
+import { useContext } from 'react';
 import { IProduct } from './models';
+import { ModalContext } from './context/ModalContext';
+
 // import {products} from './data/product'
 
 function App() {
   const { loading, products, error, addProduct } = useProducts()
-  const [modal, setModal] = useState(true)
-  const createHandler = (product: IProduct) =>{
-    setModal(false)
+  const { modal, open, close} = useContext(ModalContext)
+    const createHandler = (product: IProduct) =>{
+    close()
     addProduct(product)
   }
 
@@ -22,9 +24,13 @@ function App() {
       { error && <ErrorMessage error = {error}/>  }
       { products.map(product => <Product product={product} key={product.id}/>) }
       
-      {modal && <Modal title='Create new product '>
+      {modal && <Modal title='Create new product ' onClose={close}>
         <CreateProduct onCreate={createHandler}/>
       </Modal>}
+
+      <button className='fixed bottom-5 right-5 rounded-full bg-red-700 text-white text-2xl px-4 py-2'
+      onClick={open}
+      >+</button>
     </div>
   );
  }
--- ReactJS/my-app/src/components/Modal.tsx
+++ ReactJS/my-app/src/components/Modal.tsx
@@ -3,15 +3,14 @@ import React from "react";
 interface ModalProps{
     children: React.ReactNode
     title: string
+    onClose: () => void
 }
 
 
-export function Modal({children, title}: ModalProps) {
+export function Modal({children, title, onClose}: ModalProps) {
     return (
         <>
-        <div
-        className="fixed bg-black/50 top-0 right-0 left-0 bottom-0"
-        />
+        <div className="fixed bg-black/50 top-0 right-0 left-0 bottom-0" onClick={onClose}/>
         <div
         className="w-[500px] p-5 rounded bg-white absolute top-10 left-1/2 -translate-x-1/2"
         >
