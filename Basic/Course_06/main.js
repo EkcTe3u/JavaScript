8b9be28d69b1b82eaab851ef781878e6a38afce0
Andrey <andreysozdatelev@gmail.com>
2023-02-11 21:32:24 +0300

    attributes

--- Basic/Course_06/main.js
+++ Basic/Course_06/main.js
@@ -59,3 +59,28 @@ heading.classList.toggle("red-text");
 heading.classList.toggle("green");
 
 heading.classList.contains("green");
+
+// ================= атрибуты ================
+
+/*
+getAttribute(attrName) — возвращает значение указанного атрибута
+setAttribute(name, value) — добавляет указанные атрибут и его значение к элементу
+hasAttribute(attrName) — возвращает true при наличии у элемента указанного атрибута
+removeAttribute(attrName) — удаляет указанный атрибут
+
+toggleAttribute(name, force) — добавляет новый атрибут при отсутствии или удаляет существующий атрибут.
+hasAttributes() — возвращает true, если у элемента имеются какие-либо атрибуты
+getAttributesNames() — возвращает названия атрибутов элемента
+*/
+
+const img = document.querySelector("#logo");
+img.getAttribute("src");
+
+img.setAttribute("src", "./img/php.png");
+img.setAttribute("width", "200");
+img.setAttribute("src", "./img/js.png");
+
+img.src = "./img/js.png";
+
+const button = document.querySelector('#button')
+button.setAttribute('value', 'Send')
