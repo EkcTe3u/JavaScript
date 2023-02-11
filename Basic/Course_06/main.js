ba19d54be517124a61512bc0a4b124318d87cc19
Andrey <andreysozdatelev@gmail.com>
2023-02-11 18:08:13 +0300

    querySelector DOM elements

--- Basic/Course_06/main.js
+++ Basic/Course_06/main.js
@@ -1,29 +1,37 @@
-// classes
-class Person {
-  constructor(userName, age, isMarried) {
-    this.userName = userName;
-    this.age = age;
-    this.isMarried = isMarried;
-  }
+// // classes
+// class Person {
+//   constructor(userName, age, isMarried) {
+//     this.userName = userName;
+//     this.age = age;
+//     this.isMarried = isMarried;
+//   }
 
-  sayHi(yourName) {
-    console.log(`Привет, ${yourName}! Меня зовут ${this.userName}.`);
-  }
-}
+//   sayHi(yourName) {
+//     console.log(`Привет, ${yourName}! Меня зовут ${this.userName}.`);
+//   }
+// }
 
-// Теперь создадим новые объекты на основе класса person
+// // Теперь создадим новые объекты на основе класса person
 
-const person1 = new Person("Марк", 30, false);
-const person2 = new Person("Павел", 28, true);
+// const person1 = new Person("Марк", 30, false);
+// const person2 = new Person("Павел", 28, true);
 
-console.log(person1);
-console.log(person2);
+// console.log(person1);
+// console.log(person2);
 
 // Они были созданы с указанными свойствами, плюс оба имеют методы которые были описаны в конструкторе
 
-person1.sayHi("Юрий");
-person2.sayHi("Елена");
-
-
+// person1.sayHi("Юрий");
+// person2.sayHi("Елена");
 
 // ================= DOM elements ================
+
+// document.querySelector("h1");
+// document.querySelector(".header");
+// document.querySelector("#footer");
+
+const header = document.querySelector("h2");
+
+console.log(header);
+document.querySelector("h3").classList.add("red");
+document.querySelector("h3").classList.add("blue");
