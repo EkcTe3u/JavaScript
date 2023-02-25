86f3e45f971530fc21efec151b8d4c485effb1b4
Andrey <andreysozdatelev@gmail.com>
2023-02-25 22:50:45 +0300

    practice

--- Basic/Course_06/main.js
+++ Basic/Course_06/main.js
@@ -368,3 +368,76 @@ async function startPromise() {
 }
 
 startPromise();
+
+
+// ============ Practice ============
+
+function checkRooms() {
+	return new Promise(function (resolve, reject) {
+		setTimeout(function () {
+			console.log('Проверяем номера в отеле...');
+			// ---- код который отправляет запрос в отель ---
+			let availableRooms = true;
+
+			if (availableRooms) {
+				resolve('Номера есть!');
+			} else {
+				reject('Номеров нет.');
+			}
+		}, 1500);
+	});
+}
+
+function checkTickets(data) {
+	return new Promise(function (resolve, reject) {
+		setTimeout(() => {
+			console.log('----  checkTickets ----');
+			console.log('Ответ на предыдущем шаге:', data);
+
+			console.log('Проверяем авиабилеты...');
+			// ---- код который отправляет запрос в авиакомпанию ---
+			const availableTickets = true;
+
+			if (availableTickets) {
+				let message = 'Билеты есть';
+				resolve(message);
+			} else {
+				let message = 'Билетов нет';
+				reject(message);
+			}
+		}, 1000);
+	});
+}
+
+function submitVacation(data) {
+	console.log('---- submitVacation ----');
+	console.log('Ответ на предыдущем шаге:', data);
+	console.log('Едем в отпуск! 🌴 :)');
+}
+
+function cancelVacation(data) {
+	console.log('---- cancelVacation ----');
+	console.log('Ответ на предыдущем шаге:', data);
+	console.log('Отпуск отменяется 😢 :(');
+}
+
+async function checkVacation() {
+	// const roomsResult = await checkRooms();
+	// const ticketsResult = await checkTickets(roomsResult);
+
+	// if (ticketsResult === 'Билеты есть') {
+	// 	submitVacation(ticketsResult);
+	// } else {
+	// 	cancelVacation(ticketsResult);
+	// }
+
+	try {
+		const roomsResult = await checkRooms();
+		const ticketsResult = await checkTickets(roomsResult);
+		submitVacation(ticketsResult);
+	} catch (err) {
+		cancelVacation(err);
+	}
+}
+
+checkVacation();
