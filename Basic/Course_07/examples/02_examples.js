db363aba4e910509a18ed5c734037afcb910d1a7
Andrey <andreysozdatelev@gmail.com>
2023-03-10 21:10:05 +0300

    2nd

---
+++ Basic/Course_07/examples/02_examples.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const adder = (initial = 0) => ({
+  value: initial,
+  steps: [initial],
+  add(value) {
+    this.steps.push(value);
+    this.value += value;
+    return this;
+  },
+});
+
+const Adder = class { // классы с большой буквы
+  constructor(initial = 0) {
+    this.steps = [initial];
+    this.value = initial;
+  }
+
+  add(value) {
+    this.steps.push(value);
+    this.value += value;
+    return this;
+  }
+};
+
+// Usage
+
+{
+  const { value, steps } = adder(5).add(-8).add(11);
+  console.log(value);
+  const [a, b, c] = steps;
+  console.log(a, b, c);
+}
+
+{
+  const { value, steps } = new Adder(5).add(-8).add(11);
+  console.log(value);
+  const [a, b, c] = steps;
+  console.log(a, b, c);
+}
