e9cf17aea2d7a9809d0a2d265f8ff527d3263066
Andrey <andreysozdatelev@gmail.com>
2023-03-11 10:54:49 +0300

    delete

--- Basic/Course_07/02_loops.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const MAX_VALUE = 10;
-
-console.log('Begin');
-for (let i = 0; i < MAX_VALUE; i++) {
-  console.dir({ i, date: new Date() });
-}
-console.log('The end');
--- Basic/Course_07/03_functions.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const add = (a, b) => a + b;
-
-console.log('Add numbers: 5 + 2 = ' + add(5, 2));
-console.log('Add floats: 5.1 + 2.3 = ' + add(5.1, 2.3));
-console.log(`Concatenate: '5' + '2' = '${add('5', '2')}'`);
-console.log('Subtraction: 5 + (-2) = ' + add(5, -2));
