2119a64e7d0dc7dad349ad08777fa2bd9d734609
Andrey <andreysozdatelev@gmail.com>
2023-03-11 10:53:02 +0300

    change structure

---
+++ Basic/Course_07/Part_01/05_array.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const welcome = (person) => {
+  console.log(`Ave, ${person.name}!`);
+};
+
+const persons = [
+  { name: 'Marcus Aurelius' },
+  { name: 'Mao Zedong' },
+  { name: 'Rene Descartes' },
+];
+
+for (const person of persons) welcome(person);
---
+++ Basic/Course_07/Part_01/examples/01_example.js
@@ -0,0 +1,44 @@
+'use strict';
+
+// Constants
+
+const SALUTATION = 'Ave';
+
+const COLORS = [
+  /* 0 */ 'black',
+  /* 1 */ 'red',
+  /* 2 */ 'green',
+  /* 3 */ 'yellow',
+  /* 4 */ 'blue',
+  /* 5 */ 'magenta',
+  /* 6 */ 'cyan',
+  /* 7 */ 'white',
+];
+
+// Functions
+
+const colorer = (s, color) => `\x1b[3${color}m${s}\x1b[0m`; // \x1b - обозначает символ ESC
+
+const colorize = (name) => {
+  let res = '';
+  const letters = name.split('');
+  let color = 0;
+  for (const letter of letters) {
+    res += colorer(letter, color++);
+    if (color > COLORS.length) color = 0;
+  }
+  return res;
+};
+
+const greetings = (name) =>
+  name.includes('Augustus')
+    ? `${SALUTATION}, ${colorize(name)}!`
+    : `Hello, ${name}!`;
+
+// Usage
+
+const fullName = 'Marcus Aurelius Antoninus Augustus';
+console.log(greetings(fullName));
+
+const shortName = 'Marcus Aurelius';
+console.log(greetings(shortName));
