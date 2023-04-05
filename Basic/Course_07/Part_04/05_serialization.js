c0a4a22d9abb5707c5697dc14b2e32c4c4953479
Andrey <andreysozdatelev@gmail.com>
2023-04-05 20:29:59 +0300

    serialization

---
+++ Basic/Course_07/Part_04/05_serialization.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const person = {
+  name: 'Marcus',
+  city: 'Roma',
+  born: 121,
+};
+
+const s = JSON.stringify(person); // массив в строку
+console.log(s);
+
+const obj = JSON.parse(s);
+console.dir(obj);
+
+console.log('person === obj is ', person === obj);
+
+const letters = ['A', 'B', 'C', 'D'];
+console.log(letters.toString());
+console.log(letters + '');
+console.log(JSON.stringify(letters));
+console.log(letters.join('---'));
