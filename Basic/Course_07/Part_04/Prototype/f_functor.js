467190698c66c69a1e0680c300bc0c926bd2527f
Andrey <andreysozdatelev@gmail.com>
2023-04-11 21:13:31 +0300

    functor

---
+++ Basic/Course_07/Part_04/Prototype/f_functor.js
@@ -0,0 +1,70 @@
+'use strict';
+
+function Functor() {}
+
+Functor.prototype.method = function() {
+  console.log('Functor.prototype.method');
+  console.dir({
+    property: this.property,
+    method: this.method,
+  });
+  this(); // you can call this as a function
+};
+
+const factory = () => {
+
+  const functor = function() {
+    console.log('functor');
+    functor.property = 'value';
+    console.dir({
+      property: functor.property,
+      method: functor.method,
+    });
+  };
+
+  Object.setPrototypeOf(functor, Functor.prototype);
+  return functor;
+
+};
+
+// Usage
+
+const fn = factory();
+fn();
+fn.method();
+
+
+// part 2
+
+// class Functor extends Function {
+//   method() {
+//     console.log('Functor.prototype.method');
+//     console.dir({
+//       property: this.property,
+//       method: this.method,
+//     });
+//     this(); // you can call this as a function
+//   }
+// }
+
+// const factory = () => {
+
+//   const functor = function() {
+//     console.log('functor');
+//     functor.property = 'value';
+//     console.dir({
+//       property: functor.property,
+//       method: functor.method,
+//     });
+//   };
+
+//   Object.setPrototypeOf(functor, Functor.prototype);
+//   return functor;
+
+// };
+
+// // Usage
+
+// const fn = factory();
+// fn();
+// fn.method();
