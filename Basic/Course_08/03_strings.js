4d25f6e4f61af24ea4f65c1038eebe9564dd5289
Andrey <andreysozdatelev@gmail.com>
2023-08-22 21:23:14 +0300

    strings

---
+++ Basic/Course_08/03_strings.js
@@ -0,0 +1,18 @@
+const name = 'Andrey';
+const age = '38';
+
+const output = `Hello my name is ${name} and my age is ${age}`;
+console.log(output);
+
+const firstName = 'Andrey';
+console.log(firstName.length);
+console.log(firstName.toUpperCase());
+console.log(firstName.toLowerCase());
+console.log(firstName.charAt(2));
+console.log(firstName.indexOf('r'));
+console.log(firstName.startsWith('A'));
+console.log(firstName.endsWith('ey'));
+console.log(firstName.repeat(2));
+
+const password = `  my login pass`;
+console.log(password.trim());
