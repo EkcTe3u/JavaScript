3ea140898d1c67c3f6dfb13d46b6e50f173d0b3a
Andrey <andreysozdatelev@gmail.com>
2024-01-21 17:20:51 +0300

    branching

---
+++ Basic/Course_09/04_branching.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const calculateSubtotal = (goods) => {
+  let amount = 0;
+  for (const item of goods) {
+    if (item.price < 0) throw 'Negative price';
+    amount += item.price;
+  }
+  return amount;
+};
+
+const goods = [
+  { name: 'Laptop', price: 1500 },
+  { name: 'Mouse', price: 25 },
+  { name: 'Keyboard', price: 100 },
+  { name: 'HDMI cable', price: 10 },
+];
+
+const total = calculateSubtotal(goods);
+console.log({ goods, total });
+
+// тернарный оператор
+//              true      false
+//  условие ? значение : значение
--- Basic/Course_09/readme.md
+++ Basic/Course_09/readme.md
@@ -1,1 +1,2 @@
 _[JavaScript 2024 для начинающих 🧑‍💻](https://www.youtube.com/watch?v=HetL0ETbN6Y)_
+_[JavaScript for beginners code](https://github.com/HowProgrammingWorks/Lowcode)_
