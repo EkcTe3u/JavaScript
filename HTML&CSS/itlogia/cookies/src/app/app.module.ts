d80cd24d549bacea40d0a386ceed16a9c4f3df87
Andrey <andreysozdatelev@gmail.com>
2024-10-26 19:13:26 +0300

    lesson6

--- HTML&CSS/itlogia/cookies/src/app/app.module.ts
+++ HTML&CSS/itlogia/cookies/src/app/app.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, ReactiveFormsModule, AppRoutingModule],
+  imports: [BrowserModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
